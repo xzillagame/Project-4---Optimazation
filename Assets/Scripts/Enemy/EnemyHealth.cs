38fff48565d43bb9d11f52fca60691358bd32bf5
Xavian Escamilla <xzillagaming@gmail.com>
2024-10-02 19:44:13 -0500

    Init Commit with Enemy Object Pool

---
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+    public float sinkSpeed = 2.5f;
+    public int scoreValue = 10;
+    public AudioClip deathClip;
+    [HideInInspector] public EnemyManager enemyManagerPool;
+
+
+    Animator anim;
+    AudioSource enemyAudio;
+    ParticleSystem hitParticles;
+    CapsuleCollider capsuleCollider;
+    bool isDead;
+    bool isSinking;
+
+
+    void Awake ()
+    {
+        anim = GetComponent <Animator> ();
+        enemyAudio = GetComponent <AudioSource> ();
+        hitParticles = GetComponentInChildren <ParticleSystem> ();
+        capsuleCollider = GetComponent <CapsuleCollider> ();
+
+        currentHealth = startingHealth;
+    }
+
+
+    void Update ()
+    {
+        if(isSinking)
+        {
+            transform.Translate (-Vector3.up * sinkSpeed * Time.deltaTime);
+        }
+    }
+
+
+    public void TakeDamage (int amount, Vector3 hitPoint)
+    {
+        if(isDead)
+            return;
+
+        enemyAudio.Play ();
+
+        currentHealth -= amount;
+            
+        hitParticles.transform.position = hitPoint;
+        hitParticles.Play();
+
+        if(currentHealth <= 0)
+        {
+            Death ();
+        }
+    }
+
+
+    void Death ()
+    {
+        isDead = true;
+
+        capsuleCollider.isTrigger = true;
+
+        anim.SetTrigger ("Dead");
+
+        enemyAudio.clip = deathClip;
+        enemyAudio.Play ();
+    }
+
+
+    public void StartSinking()
+    {
+        Debug.Log(" test");
+        GetComponent<UnityEngine.AI.NavMeshAgent>().enabled = false;
+        GetComponent<Rigidbody>().isKinematic = true;
+        isSinking = true;
+        ScoreManager.score += scoreValue;
+        
+        StartCoroutine(AddToQueueAfterSinkRoutine());
+
+    }
+
+
+    private IEnumerator AddToQueueAfterSinkRoutine()
+    {
+        yield return new WaitForSeconds(2f);
+
+        enemyManagerPool.ReAddToQueue(this.gameObject);
+    }
+
+}
---
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class EnemyManager : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+    public GameObject enemy;
+    public float spawnTime = 3f;
+    public Transform[] spawnPoints;
+
+    public int spawnCount = 10;
+
+    private Queue<GameObject> enemyPrefabPool = new Queue<GameObject>();
+
+
+
+    void Start ()
+    {
+        EnemyHealth enemyPrefabHealthComponent = enemy.GetComponent<EnemyHealth>();
+
+        for(int i = 0; i < spawnCount; i++)
+        {
+            EnemyHealth pooledEnemy = Instantiate<EnemyHealth>(enemyPrefabHealthComponent);
+            pooledEnemy.enemyManagerPool = this;
+            enemyPrefabPool.Enqueue(pooledEnemy.gameObject);
+            pooledEnemy.gameObject.SetActive(false);
+        }
+
+
+        InvokeRepeating (nameof(Spawn), spawnTime, spawnTime);
+    }
+
+
+    void Spawn ()
+    {
+        if (playerHealth.currentHealth <= 0f) return;
+
+        int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+
+        if(enemyPrefabPool.Count > 0)
+        {
+            GameObject spawnedEnemy = enemyPrefabPool.Dequeue();
+            spawnedEnemy.SetActive(true);
+            enemy.transform.position = spawnPoints[spawnPointIndex].position;
+            enemy.transform.rotation = spawnPoints[spawnPointIndex].rotation;
+        }
+
+    }
+
+
+    public void ReAddToQueue(GameObject enemyPrefab)
+    {
+        enemyPrefabPool.Enqueue(enemyPrefab);
+    }
+
+}
